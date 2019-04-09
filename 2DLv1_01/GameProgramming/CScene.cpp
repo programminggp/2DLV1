02660b34bd230b062ba39fa45e332b9adbfb6fd3
Takahashi-Shinobu-Fukuyama <shinobu-takahashi@anabuki.ac.jp>
2019-04-09 15:10:00 +0900

    02

--- 2DLv1_01/GameProgramming/CScene.cpp
+++ 2DLv1_01/GameProgramming/CScene.cpp
@@ -5,6 +5,8 @@
 #include "CScene.h"
 //printf関数のインクルード
 #include <stdio.h>
+//gl関数のインクルード
+#include "glut.h"
 
 //一度だけ実行されるInitメソッドの定義
 void CScene::Init() {
@@ -18,4 +20,22 @@ void CScene::Init() {
 //繰り返し実行されるUpdateメソッドの定義
 void CScene::Update() {
 	printf("Update()\n");
+	/*
+	四角形の描画
+	*/
+	//描画指定の開始
+	//GL_QUADS 四角形の指定
+	glBegin(GL_QUADS);
+	/*
+	頂点座標の指定
+	glVertex2f(x,y)
+	x：X座標
+	y：Y座標
+	*/
+	glVertex2f(-50.0f, 50.0f);
+	glVertex2f(-50.0f, -50.0f);
+	glVertex2f(50.0f, -50.0f);
+	glVertex2f(50.0f, 50.0f);
+	//描画指定の終了
+	glEnd();
 }
