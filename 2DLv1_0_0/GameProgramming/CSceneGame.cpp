8747789ca2175f80dda386560ac71999840130d4
snb-takahashi <snb.takahashi@gmail.com>
2019-07-11 00:21:36 +0900

    2.変数

--- 2DLv1_0_0/GameProgramming/CSceneGame.cpp
+++ 2DLv1_0_0/GameProgramming/CSceneGame.cpp
@@ -9,13 +9,37 @@ Updateメソッド
 void CSceneGame::Update() {
 	//命令の記述
 	/*
-	長方形を描画するメソッドを呼ぶ
-	中心座標(80,60)、幅160、高さ120
+	変数の作成
+	x:長方形の中心x座標
+	y:長方形の中心y座標 
+	w:長方形の幅の半分 
+	h:長方形の高さの半分
 	*/
-	CRectangle::Render(80, 60, 80, 60);
-	//長方形の追加
-	CRectangle::Render(-80, -60, 80, 60);
-	CRectangle::Render(-80-160, 60, 80, 60);
-	CRectangle::Render(80 + 160, -60, 80, 60);
+	float x;	//float型の変数xを作成する
+	float y = 270.0;	//float型の変数yを作成し150.0を代入する
+	float w, h = 30.0;	//float型の変数wと変数hを,区切りで一度に作成する
+
+	x = 0.0;	//変数xに値を代入する
+	w = 400.0;	//変数wに値を代入する
+
+	//上の長方形を描画する
+	//変数に代入されている値で命令を実行する
+	CRectangle::Render(x, y, w, h);
+
+/*	//下の長方形を描画する
+	y = -270.0;	//代入されると前の値は上書きされる
+	CRectangle::Render(x, y, w, h);
+
+	//左の長方形を描画する
+	x = -370.0;
+	w = 30.0;
+	y = 0.0;
+	h = 300.0;
+	CRectangle::Render(x, y, w, h);
+
+	//右の長方形を描画する
+	x = 370.0;
+	CRectangle::Render(x, y, w, h);
+	*/
 }
 
