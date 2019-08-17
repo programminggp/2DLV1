216f3202dbd272a24b8b3c0c41f52d3661852f65
Takahashi-Shinobu-Fukuyama <shinobu-takahashi@anabuki.ac.jp>
2019-08-17 20:35:04 +0900

    CTriangle CVector CRectangle CMatrix operator*

--- 3DLv1_00/GameProgramming/CSceneGame.cpp
+++ 3DLv1_00/GameProgramming/CSceneGame.cpp
@@ -1,38 +1,33 @@
 #include "CSceneGame.h"
 #include "glut.h"
-#include "CVector.h"
+#include "CTriangle.h"
+
+//CSceneGame::CSceneGame() {}
 
 void CSceneGame::Init() {
-
+	mCameraEye.Set(1.0f, 3.0f, 5.0f);
+	mCameraCenter.Set(0.0f, 0.0f, 0.0f);
+	mCameraUp.Set(0.0f, 1.0f, 0.0f);
+	mTriangle[0].SetVertex(0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f);
+	mTriangle[0].SetNormal(0.0f, 0.0f, 1.0f);
+	mTriangle[1].SetVertex(0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f);
+	mTriangle[1].SetNormal(0.0f, 0.0f, 1.0f);
+	mRectangle.SetVertex(0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f, 1.0f, -1.0f, 0.0f, 1.0f, -1.0f);
+	mRectangle.SetNormal(0.0f, 1.0f, 0.0f);
+	mDegree = 0.0f;
 }
 
 void CSceneGame::Update() {
-	gluLookAt(2.0f, 3.0f, 10.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
+	mDegree++;
+	mMatrix.RotateY(mDegree);
+	gluLookAt(	mCameraEye.mX, mCameraEye.mY, mCameraEye.mZ, 
+				mCameraCenter.mX, mCameraCenter.mY, mCameraCenter.mZ,
+				mCameraUp.mX, mCameraUp.mY, mCameraUp.mZ);
 	float mDiffuse[4] = { 1.0f, 1.0f, 1.0f, 1.0f };
 	glMaterialfv(GL_FRONT, GL_DIFFUSE, mDiffuse);
-	glBegin(GL_TRIANGLES);
-	glVertex3f(0.0f, 0.0f, 0.0f);
-	glVertex3f(1.0f, 0.0f, 0.0f);
-	glVertex3f(1.0f, 1.0f, 0.0f);
-	glVertex3f(0.0f, 0.0f, -5.0f);
-	glVertex3f(1.0f, 0.0f, -5.0f);
-	glVertex3f(1.0f, 1.0f, -5.0f);
-	glVertex3f(0.0f, 0.0f, 5.0f);
-	glVertex3f(1.0f, 0.0f, 5.0f);
-	glVertex3f(1.0f, 1.0f, 5.0f);
-	glVertex3f(5.0f, 0.0f, 0.0f);
-	glVertex3f(6.0f, 0.0f, 0.0f);
-	glVertex3f(6.0f, 1.0f, 0.0f);
-	glVertex3f(-5.0f, 0.0f, 0.0f);
-	glVertex3f(-4.0f, 0.0f, 0.0f);
-	glVertex3f(-4.0f, 1.0f, 0.0f);
-	glVertex3f(0.0f, 5.0f, 0.0f);
-	glVertex3f(1.0f, 5.0f, 0.0f);
-	glVertex3f(1.0f, 6.0f, 0.0f);
-	glVertex3f(0.0f, -5.0f, 0.0f);
-	glVertex3f(1.0f, -5.0f, 0.0f);
-	glVertex3f(1.0f, -4.0f, 0.0f);
-	glEnd();
+	mTriangle[0].Render(mMatrix);
+	mTriangle[1].Render(mMatrix);
+	mRectangle.Render(mMatrix);
 }
 
 //デストラクタ
--- 3DLv1_00/GameProgramming/CSceneGame.h
+++ 3DLv1_00/GameProgramming/CSceneGame.h
@@ -1,6 +1,10 @@
 #ifndef CSCENEGAME_H
 #define CSCENEGAME_H
 #include "CScene.h"
+#include "CVector.h"
+#include "CTriangle.h"
+#include "CRectangle.h"
+#include "CMatrix.h"
 
 /*
 ゲームのシーン
@@ -8,12 +12,20 @@
 //CSceneクラスを継承する
 class CSceneGame : public CScene {
 public:
+	CVector mCameraEye;
+	CVector mCameraCenter;
+	CVector mCameraUp;
+
+	CTriangle mTriangle[2];
+	CRectangle mRectangle;
+	float mDegree;
+	CMatrix mMatrix;
 
 	//初期化処理のオーバーライド
 	void Init();
 	//更新処理のオーバーライド
 	void Update();
-
+//	CSceneGame();
 	//デストラクタの宣言
 	~CSceneGame();
 };
