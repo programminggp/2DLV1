397c670760130097e16c5bb8951b81255e915d97
Takahashi-Shinobu-Fukuyama <shinobu-takahashi@anabuki.ac.jp>
2019-09-12 18:23:36 +0900

    2.1 CVector.h

--- 3DLv1_00_2019/GameProgramming/CSceneGame.cpp
+++ 3DLv1_00_2019/GameProgramming/CSceneGame.cpp
@@ -1,13 +1,28 @@
 #include "CSceneGame.h"
 //OpenGL
 #include "glut.h"
+//CVectorクラスのインクルード
+#include "CVector.h"
 
 void CSceneGame::Init() {
 }
 
 void CSceneGame::Update() {
+	//頂点1､頂点2､頂点3,法線データの作成
+	CVector v0, v1, v2, n;
+
+	//法線を上向きで設定する
+	n.mX = 0.0f; n.mY = 1.0f; n.mZ = 0.0f;
+
+	//頂点1の座標を設定する
+	v0.mX = 0.0f; v0.mY = 0.0f; v0.mZ = 0.5f;
+	//頂点2の座標を設定する
+	v1.mX = 1.0f; v1.mY = 0.0f; v1.mZ = 0.0f;
+	//頂点3の座標を設定する
+	v2.mX = 0.0f; v2.mY = 0.0f; v2.mZ = -0.5f;
+
 	//視点の設定
-	//gluLookAt(視点X, 視点Y, 視点Z, 目標X, 目標Y, 目標Z, 上向X, 上向Y, 上向Z)
+	//gluLookAt(視点X, 視点Y, 視点Z, 中心X, 中心Y, 中心Z, 上向X, 上向Y, 上向Z)
 	gluLookAt(1.0f, 2.0f, 3.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
 
 	//描画開始
@@ -17,25 +32,35 @@ void CSceneGame::Update() {
 
 	//法線（面の向き）の設定
 	//glNormal3f(X座標, Y座標, Z座標)
-	glNormal3f(0.0f, 1.0f, 0.0f);
-
+	glNormal3f(n.mX, n.mY, n.mZ);
 	//頂点座標の設定
 	//glVertex3f(X座標, Y座標, Z座標)
-	glVertex3f(0.0f, 0.0f, 0.0f);
-	glVertex3f(1.0f, 0.0f, 0.0f);
-	glVertex3f(0.0f, 0.0f, -0.5f);
+	glVertex3f(v0.mX, v0.mY, v0.mZ);
+	glVertex3f(v1.mX, v1.mY, v1.mZ);
+	glVertex3f(v2.mX, v2.mY, v2.mZ);
+	/*
+	//法線と頂点の設定
+	n.mY = 0.0f; n.mZ = 1.0f;
+	v0.Set(0.5f, 0.0f, 0.0f);
+	v1.Set(0.0f, 1.0f, 0.0f);
+	v2.Set(-0.5f, 0.0f, 0.0f);
+	//三角形2の描画
+	glNormal3f(n.mX, n.mY, n.mZ);
+	glVertex3f(v0.mX, v0.mY, v0.mZ);
+	glVertex3f(v1.mX, v1.mY, v1.mZ);
+	glVertex3f(v2.mX, v2.mY, v2.mZ);
 
-	//面の向きはZ軸方向
-	glNormal3f(0.0f, 0.0f, 1.0f);
-	glVertex3f(0.0f, 0.0f, 0.0f);
-	glVertex3f(0.0f, 1.0f, 0.0f);
-	glVertex3f(-0.5f, 0.0f, 0.0f);
-
-	glNormal3f(1.0f, 0.0f, 0.0f);
-	glVertex3f(0.0f, 0.0f, 0.0f);
-	glVertex3f(0.0f, 0.0f, 1.0f);
-	glVertex3f(0.0f, -0.5f, 0.0f);
-
+	//法線と頂点の設定
+	n.mX = 1.0f; n.mZ = 0.0f;
+	v0.Set(0.0f, 0.5f, 0.0f);
+	v1.Set(0.0f, 0.0f, 1.0f);
+	v2.Set(0.0f, -0.5f, 0.0f);
+	//三角形3の描画
+	glNormal3f(n.mX, n.mY, n.mZ);
+	glVertex3f(v0.mX, v0.mY, v0.mZ);
+	glVertex3f(v1.mX, v1.mY, v1.mZ);
+	glVertex3f(v2.mX, v2.mY, v2.mZ);
+*/
 	//描画終了
 	glEnd();
 }
---
+++ 3DLv1_00_2019/GameProgramming/CVector.cpp
@@ -0,0 +1,15 @@
+#include "CVector.h"
+/*
+//コンストラクタ
+//各値を0に初期化
+CVector::CVector()
+: mX(0.0f), mY(0.0f), mZ(0.0f) {}
+
+//Set(X座標, Y座標, Z座標)
+void CVector::Set(float x, float y, float z) {
+	//各軸の値を代入します
+	mX = x;
+	mY = y;
+	mZ = z;
+}
+*/
