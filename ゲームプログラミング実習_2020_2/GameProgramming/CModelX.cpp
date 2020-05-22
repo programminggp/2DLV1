6e09838123c90497ee17bb592bac1637a806235c
Takahashi-Shinobu-Fukuyama <shinobu-takahashi@anabuki.ac.jp>
2020-05-22 15:02:07 +0900

    02

--- ゲームプログラミング実習_2020_2/GameProgramming/CModelX.cpp
+++ ゲームプログラミング実習_2020_2/GameProgramming/CModelX.cpp
@@ -1,5 +1,6 @@
 #include "glut.h"
 #include <stdio.h>
+#include <string.h>	//文字列関数のインクルード
 #include "CModelX.h"
 
 void CModelX::Load(char *file) {
@@ -29,7 +30,64 @@ void CModelX::Load(char *file) {
 	buf[size] = '\0';
 	fclose(fp);	//ファイルをクローズする
 
-	printf("%s", buf);
+//	printf("%s", buf);
+	//文字列の最後まで繰り返し
+	while (*mpPointer != '\0') {
+		GetToken();	//単語の取得
+		//単語がFrameの場合
+		if (strcmp(mToken, "Frame") == 0) {
+			printf("%s ", mToken);	//Frame出力
+			GetToken();	//Frame名を取得
+			printf("%s\n", mToken);	//Frame名を出力
+		}
+		if (strcmp(mToken, "AnimationSet") == 0) {
+			printf("%s ", mToken);	//Frame出力
+			GetToken();	//Frame名を取得
+			printf("%s\n", mToken);	//Frame名を出力
+		}
+	}
 
 	SAFE_DELETE_ARRAY(buf);	//確保した領域を開放する
+}
+
+/*
+GetToken
+文字列データから、単語を1つ取得する
+*/
+void CModelX::GetToken() {
+	char* p = mpPointer;
+	char* q = mToken;
+	//空白( )タブ(\t)改行(\r)(\n)，；”以外の文字になるまで読み飛ばす
+	/*
+	strchr(文字列, 文字)
+	文字列に文字が含まれていれば、見つかった文字へのポインタを返す
+	見つからなかったらNULLを返す
+	*/
+	while (*p != '\0' && strchr(" \t\r\n,;\"", *p)) p++;
+	if (*p == '{' || *p == '}') {
+		//{または}ならmTokenに代入し次の文字へ
+		*q++ = *p++;
+	}
+	else {
+		//空白( )タブ(\t)改行(\r)(\n)，；”}の文字になるまでmTokenに代入する
+		while (*p != '\0' && !strchr(" \t\r\n,;\"}", *p))
+			*q++ = *p++;
+	}
+	*q = '\0';	//mTokenの最後に\0を代入
+	mpPointer = p;	//次の読み込むポイントを更新する
+
+	//もしmTokenが//の場合は、コメントなので改行まで読み飛ばす
+	/*
+	strcmp(文字列1, 文字列2)
+	文字列1と文字列2が等しい場合、0を返します。
+	文字列1と文字列2が等しい場合、0以外を返します。
+	*/
+	if (!strcmp("//", mToken)) {
+		//改行まで読み飛ばす
+		while (*p != '\0' && !strchr("\r\n", *p)) p++;
+		//読み込み位置の更新
+		mpPointer = p;
+		//単語を取得する（再帰呼び出し）
+		GetToken();
+	}
 }
