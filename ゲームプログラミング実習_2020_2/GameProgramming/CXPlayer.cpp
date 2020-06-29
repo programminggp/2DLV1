a731c12fc79b66b409d89ba2ccd895e9d1253f1d
Takahashi-Shinobu-Fukuyama <shinobu-takahashi@anabuki.ac.jp>
2020-06-29 17:55:24 +0900

    22

--- ゲームプログラミング実習_2020_2/GameProgramming/CXPlayer.cpp
+++ ゲームプログラミング実習_2020_2/GameProgramming/CXPlayer.cpp
@@ -9,9 +9,17 @@ CXPlayer::CXPlayer() {
 
 void CXPlayer::Update() {
 
+	if (CKey::Push('A')) {
+		mRotation.mY += 2;
+	}
+	if (CKey::Push('D')) {
+		mRotation.mY -= 2;
+	}
+
 	if (CKey::Push('W')) {
 		ChangeAnimation(1, true, 60);
-		mPosition.mZ += 0.1f;
+		mPosition = mPosition + CVector(0.0f, 0.0f, 0.1f) * mMatrixRotate;
+//		mPosition.mZ += 0.1f;
 	}
 	else {
 		ChangeAnimation(0, true, 60);
