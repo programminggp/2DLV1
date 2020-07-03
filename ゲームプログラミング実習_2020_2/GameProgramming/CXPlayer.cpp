6c6b683b0260917077998ddeaa43b6b7807cb2f0
Takahashi-Shinobu-Fukuyama <shinobu-takahashi@anabuki.ac.jp>
2020-07-03 12:37:19 +0900

    23

--- ゲームプログラミング実習_2020_2/GameProgramming/CXPlayer.cpp
+++ ゲームプログラミング実習_2020_2/GameProgramming/CXPlayer.cpp
@@ -8,21 +8,37 @@ CXPlayer::CXPlayer() {
 }
 
 void CXPlayer::Update() {
+	switch (mAnimationIndex) {
+	case 3:
+		if (mAnimationFrame >= mAnimationFrameSize) {
+			ChangeAnimation(4, false, 30);
+		}
+		break;
+	case 4:
+		if (mAnimationFrame >= mAnimationFrameSize) {
+			ChangeAnimation(0, true, 60);
+		}
+		break;
+	default:
+		if (CKey::Push('A')) {
+			mRotation.mY += 2;
+		}
+		if (CKey::Push('D')) {
+			mRotation.mY -= 2;
+		}
 
-	if (CKey::Push('A')) {
-		mRotation.mY += 2;
-	}
-	if (CKey::Push('D')) {
-		mRotation.mY -= 2;
-	}
+		if (CKey::Push('W')) {
+			ChangeAnimation(1, true, 60);
+			mPosition = mPosition + CVector(0.0f, 0.0f, 0.1f) * mMatrixRotate;
+			//		mPosition.mZ += 0.1f;
+		}
+		else {
+			ChangeAnimation(0, true, 60);
+		}
 
-	if (CKey::Push('W')) {
-		ChangeAnimation(1, true, 60);
-		mPosition = mPosition + CVector(0.0f, 0.0f, 0.1f) * mMatrixRotate;
-//		mPosition.mZ += 0.1f;
-	}
-	else {
-		ChangeAnimation(0, true, 60);
+		if (CKey::Push(' ')) {
+			ChangeAnimation(3, false, 30);
+		}
 	}
 
 	CXCharacter::Update();
