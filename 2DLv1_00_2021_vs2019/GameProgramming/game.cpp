99d8ef4295d5aeaf976211957efb7e2cba4e6e3b
snb-takahashi <snb.takahashi@gmail.com>
2021-03-06 23:24:30 +0900

    Lesson1を最初から

--- 2DLv1_00_2021_vs2019/GameProgramming/game.cpp
+++ 2DLv1_00_2021_vs2019/GameProgramming/game.cpp
@@ -1,74 +1,38 @@
 /*
-Lesson2
-（１）デストラクタの作成
-（２）\n
+Lesson1
+CGameクラスの定義とインスタンスの作成
 課題
-（１）CLesson2クラスのデストラクタの追加
-（２）CLesson2のデストラクタの確認
+CRectクラスの定義とインスタンスの作成
+
 */
 
-#include <stdio.h> //printf_s関数を取り込む
+#include <stdio.h> //printf_s関数の取り込み
 
-/*
-class
-クラスとは小さなプログラムの作成単位です
-*/
-//クラスの定義
-//class クラス名
-class CLesson1 //CLesson1クラスの定義
-{ //クラス定義の開始
-public: //全てからのアクセスを許可
-	CLesson1(); //CLesson1クラスのデフォルトコンストラクタの宣言
-	~CLesson1(); //CLesson1クラスのデストラクタの宣言
-}; //クラス定義の終了 ;が必要なので注意
+class CGame //CGameクラスの定義
+{ //定義の開始
+public: //全てからアクセス可能
+	void Update(); //Updateメソッドの宣言
+}; //定義の終了;注意
 
-/*
-コンストラクタとは
-クラスのインスタンス（実体）が作成される時に実行される処理です
-*/
-//デフォルトコンストラクタの定義
-//クラス名::クラス名()
-CLesson1::CLesson1() //CLesson1クラスのデフォルトコンストラクタの定義
+void CGame::Update() //Updateメソッドの処理の定義
 { //処理の開始
-	//printf_s関数を使うと、文字列を出力できます
-	printf_s("CLesson1()"); //"文字列"を画面に出力（表示）する
+	printf_s("CGame::Update()\n"); //printf_s関数の呼び出し"文字列"を出力
+	//\nは改行
+
 } //処理の終了
 
-/*
-デストラクタとは
-クラスのインスタンス（実体）が削除される時に実行される処理です
-*/
-//デストラクタの定義
-//クラス名::~クラス名()
-CLesson1::~CLesson1()
-{
-	printf_s("~CLesson1()\n"); //\nは改行文字で次の行へいきます
-}
-
-class CLesson2
-{
-public:
-	CLesson2();
-};
-
-CLesson2::CLesson2()
-{
-	printf_s("CLesson2()");
-}
-
-
-CLesson1 Instance; //CLesson1クラスのインスタンス（実体）の作成
-CLesson2 Lesson2;
+CGame gGame; //CGameクラスのインスタンスの作成
 
 /*
 GameLoop関数の定義
 ゲームのメインループ
 繰り返し呼ばれる処理
 */
-void GameLoop() //GameLoop関数の定義
+void GameLoop() //GameLoop関数の処理の定義
 { //処理の開始
-	CLesson1 Instance; //CLesson1クラスのインスタンスを作成します
-	//{}の中で作成されたインスタンスは}の処理が終了すると削除されます
+
+	printf_s("GameLoop()"); //printf_s関数の呼び出し"文字列"を出力
+	gGame.Update(); //Updateメソッドの呼び出し
 
 	return; //returnすると、処理が終わります
 } //処理の終了
