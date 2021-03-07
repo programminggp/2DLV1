dcb08a7fda090f7dced5a404bab7a38b135c7265
snb-takahashi <snb.takahashi@gmail.com>
2021-03-07 15:16:05 +0900

    Lesson1 修正

--- 2DLv1_00_2021_vs2019/GameProgramming/game.cpp
+++ 2DLv1_00_2021_vs2019/GameProgramming/game.cpp
@@ -3,22 +3,28 @@ Lesson1
 CGameクラスの定義とインスタンスの作成
 課題
 CRectクラスの定義とインスタンスの作成
-
 */
 
-#include <stdio.h> //printf_s関数の取り込み
-
+#include <stdio.h> //printf関数の取り込み
+/*
+class
+クラスとはプログラムの作成単位です
+*/
+//クラスの定義
+//class クラス名
 class CGame //CGameクラスの定義
 { //定義の開始
 public: //全てからアクセス可能
-	void Update(); //Updateメソッドの宣言
+	void Render(); //Renderメソッドの宣言
 }; //定義の終了;注意
-
-void CGame::Update() //Updateメソッドの処理の定義
+/*
+メソッドとは
+プログラムの命令を集めた、クラスに作成される処理です。
+*/
+void CGame::Render() //Renderメソッドの処理の定義
 { //処理の開始
-	printf_s("CGame::Update()\n"); //printf_s関数の呼び出し"文字列"を出力
-	//\nは改行
-
+	printf("CGame::Render()\n"); //printf関数を呼び出し"文字列"を出力
+						//\nは改行
 } //処理の終了
 
 CGame gGame; //CGameクラスのインスタンスの作成
@@ -26,13 +32,14 @@ CGame gGame; //CGameクラスのインスタンスの作成
 /*
 GameLoop関数の定義
 ゲームのメインループ
-繰り返し呼ばれる処理
+ゲーム実行中繰り返し呼ばれる処理
 */
 void GameLoop() //GameLoop関数の処理の定義
 { //処理の開始
 
-	printf_s("GameLoop()"); //printf_s関数の呼び出し"文字列"を出力
-	gGame.Update(); //Updateメソッドの呼び出し
+	printf("GameLoop()\n"); //printf関数を呼び出し"文字列"を出力
+
+	gGame.Render(); //gGameのRenderメソッドの呼び出し
 
 	return; //returnすると、処理が終わります
 } //処理の終了
