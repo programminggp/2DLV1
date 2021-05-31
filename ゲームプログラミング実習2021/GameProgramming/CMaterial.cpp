b35aad4bb25b9abdd53bb1f152939402db474fdd
Takahashi-Shinobu-Fukuyama <shinobu-takahashi@anabuki.ac.jp>
2021-05-31 12:20:51 +0900

    課題９の解答

--- ゲームプログラミング実習2021/GameProgramming/CMaterial.cpp
+++ ゲームプログラミング実習2021/GameProgramming/CMaterial.cpp
@@ -1,6 +1,7 @@
 #include "CMaterial.h"
 //memsetのインクルード
 #include <string.h>
+//#include <stdio.h>
 
 //デフォルトコンストラクタ
 CMaterial::CMaterial()
@@ -93,5 +94,13 @@ CMaterial::CMaterial(CModelX* model)
 		model->GetToken(); // }
 		model->GetToken(); // }
 	}
+
+//#ifdef _DEBUG
+//	printf("%s\n", mName);
+//	printf("Diffuse:%f %f %f %f\n", mDiffuse[0], mDiffuse[1], mDiffuse[2], mDiffuse[3]);
+//	printf("Power:%f\n", mPower);
+//	printf("Specular:%f %f %f\n", mSpecular[0], mSpecular[1], mSpecular[2]);
+//	printf("Emissive:%f %f %f\n", mEmissive[0], mEmissive[1], mEmissive[2]);
+//#endif
 }
 
