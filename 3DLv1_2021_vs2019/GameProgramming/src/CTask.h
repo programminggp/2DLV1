05ba0f370ebcb2148305710ad1ef64b5019539c5
snb-takahashi <snb.takahashi@gmail.com>
2021-08-10 14:12:53 +0900

    課題２１２２完了

--- 3DLv1_2021_vs2019/GameProgramming/src/CTask.h
+++ 3DLv1_2021_vs2019/GameProgramming/src/CTask.h
@@ -1,18 +1,26 @@
 #ifndef CTASK_H
 #define CTASK_H
+
+class CTaskManager;
+class CCollisionManager;
+
 /*
 タスククラス
 タスクリストの要素
 */
 class CTask {
-public:
-	CTask *mpNext;//次のポインタ
-	CTask *mpPrev;//前のポインタ
+	friend CTaskManager;
+	friend CCollisionManager;
+private:
+	CTask* mpNext;//次のポインタ
+	CTask* mpPrev;//前のポインタ
+protected:
 	int mPriority;	//優先度
 	bool mEnabled;	//有効フラグ
+public:
 	//デフォルトコンストラクタ
 	CTask()
-		: mpNext(0), mpPrev(0), mPriority(0), mEnabled(true) {}
+		: mpNext(nullptr), mpPrev(nullptr), mPriority(0), mEnabled(true) {}
 	//デストラクタ virtualにしないと子クラスのデストラクタが呼ばれない
 	virtual ~CTask() {}
 	//更新
