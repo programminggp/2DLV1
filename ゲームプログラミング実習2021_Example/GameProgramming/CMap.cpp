d87663eb96175d60040d4a5700fd9be8abce833a
Takahashi-Shinobu-Fukuyama <shinobu-takahashi@anabuki.ac.jp>
2021-09-22 15:16:47 +0900

    Add UnderGround Model

--- ゲームプログラミング実習2021_Example/GameProgramming/CMap.cpp
+++ ゲームプログラミング実習2021_Example/GameProgramming/CMap.cpp
@@ -1,9 +1,12 @@
 #include "CMap.h"
 
+#define OBJ "UnderGround\\passage2.obj"
+#define MTL "UnderGround\\passage2.mtl"
+
 CMap::CMap()
 {
 	mpModel = &mModel;
-	mModel.Load("sky.obj", "sky.mtl");
+	mModel.Load(OBJ, MTL);
 	mPosition = CVector(0.0f, 1.0f, 0.0f);
 	mScale = CVector(1.0f, 1.0f, 1.0f);
 	CTransform::Update();
