53b042b0c18ced2b9173bde55409f1f1b2f7a4e7
Takahashi-Shinobu-Fukuyama <shinobu-takahashi@anabuki-net.ne.jp>
2021-10-19 16:23:15 +0900

    １．三角形を描画する

--- GameProgramming2_0/GameProgramming/MainLoop.cpp
+++ GameProgramming2_0/GameProgramming/MainLoop.cpp
@@ -1,27 +1,5 @@
 #include "glut.h"
 
-/*２．
-* 三角形のプログラム
-* CTriangleプログラム（クラス）を定義する
-*/
-class CTriangle
-{
-public:
-	//２．Render処理（メソッド）を宣言する
-	void Render();
-};
-//２．Render処理（メソッド）を定義する
-void CTriangle::Render()
-{
-	glBegin(GL_TRIANGLES);
-	glVertex2f(0.0f, 100.0f);
-	glVertex2f(-100.0f, -100.0f);
-	glVertex2f(100.0f, -100.0f);
-	glEnd();
-}
-//２．CTriangleクラスの変数（インスタンス）を作成する
-CTriangle Triangle;
-
 //Init関数
 //最初に一度だけ呼ばれる関数
 void Init() {
@@ -29,6 +7,10 @@ void Init() {
 //MailLoop関数
 //プログラムの実行中、繰り返し呼ばれる関数
 void MainLoop() {
-	//２．変数TriangleのRender処理を呼び出す
-	Triangle.Render();
+	//１．三角形を描画する
+	glBegin(GL_TRIANGLES);
+	glVertex2f(0.0f, 100.0f);
+	glVertex2f(-100.0f, -100.0f);
+	glVertex2f(100.0f, -100.0f);
+	glEnd();
 }
