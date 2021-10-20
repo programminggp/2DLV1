d41e977d64d015916b895684f52ca7e969a7695e
Takahashi-Shinobu-Fukuyama <shinobu-takahashi@anabuki-net.ne.jp>
2021-10-20 17:17:54 +0900

    ３．コメントと初期化変更 ４．引数付きコンストラクタ　途中

--- GameProgramming2_0/GameProgramming/MainLoop.cpp
+++ GameProgramming2_0/GameProgramming/MainLoop.cpp
@@ -11,20 +11,21 @@ class CTriangle
 public:
 	//３．デフォルトコンストラクタを宣言する
 	CTriangle();
+	//４．引数付きのコンストラクタを宣言する
+	CTriangle(float x0, float y0, float x1, float y1, float x2, float y2);
 	//２．Render処理（メソッド）を宣言する
 	void Render();
 };
 //３．デフォルトコンストラクタを定義する
 CTriangle::CTriangle()
-{
 	//３．変数に値を代入する
-	x0 = -100.0f;
-	y0 = 100.0f;
-	x1 = 0.0f;
-	y1 = -100.0f;
-	x2 = 100.0f;
-	y2 = 100.0f;
+	: x0(-100.0f), y0(100.0f), x1(0.0f), y1(-100.0f), x2(100.0f), y2(100.0f)
+{
 }
+//４．引数付きのコンストラクタを定義する
+CTriangle::CTriangle(float ax0, float ay0, float ax1, float ay1, float ax2, float ay2)
+	: x0(ax0), y0(ay0), x1(ax1), y1(ay1), x2(ax2), y2(ay2)
+{}
 //２．Render処理（メソッド）を定義する
 void CTriangle::Render()
 {
@@ -36,7 +37,7 @@ void CTriangle::Render()
 	glEnd();
 }
 //２．CTriangleクラスの変数（インスタンス）を作成する
-//３．この時デフォルトコンストラクタが実行される
+//３．この時デフォルトコンストラクタを呼び出す
 CTriangle Triangle;
 
 //Init関数
