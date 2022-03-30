8a98ba87ef723d0d137517bb0d42c5636374b0f0
Takahashi-Shinobu-Fukuyama <shinobu-takahashi@anabuki.ac.jp>
2022-03-30 13:23:27 +0900

    課題７

--- 2DLv1_2022_vs2019/GameProgramming/src/CApplication.cpp
+++ 2DLv1_2022_vs2019/GameProgramming/src/CApplication.cpp
@@ -10,6 +10,7 @@ void CApplication::Start()
 void CApplication::Update()
 {
 	mRectangle.Render();
-	mTexture.DrawImage(0.0f, 400.0f, 580.0f, 600.0f, 0, 300, 20, 0);
+//	mTexture.DrawImage(0.0f, 400.0f, 580.0f, 600.0f, 0, 300, 20, 0);
+	mTexture.DrawImage(0.0f, 52.0f, 0.0f, 88.0f, 740, 876, 1236, 1016);
 }
 
