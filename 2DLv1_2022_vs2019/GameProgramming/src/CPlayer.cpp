48da710919d7ed93ad7c343e1d2f180808ccbab0
Takahashi-Shinobu-Fukuyama <shinobu-takahashi@anabuki.ac.jp>
2022-04-22 15:13:50 +0900

    14

--- 2DLv1_2022_vs2019/GameProgramming/src/CPlayer.cpp
+++ 2DLv1_2022_vs2019/GameProgramming/src/CPlayer.cpp
@@ -7,19 +7,19 @@ void CPlayer::Update()
 		float x = X() - 4.0f;
 		X(x);
 	}
-	if (mInput.Key('W'))
-	{
-		float y = Y() + 4.0f;
-		Y(y);
-	}
+	//if (mInput.Key('W'))
+	//{
+	//	float y = Y() + 4.0f;
+	//	Y(y);
+	//}
 	if (mInput.Key('D'))
 	{
 		float x = X() + 4.0f;
 		X(x);
 	}
-	if (mInput.Key('S'))
-	{
-		float y = Y() - 4.0f;
-		Y(y);
-	}
+	//if (mInput.Key('S'))
+	//{
+	//	float y = Y() - 4.0f;
+	//	Y(y);
+	//}
 }
