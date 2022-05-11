80d9d2f2c55cc8547f1688f31a7e384432bfe4e3
Takahashi-Shinobu-Fukuyama <shinobu-takahashi@anabuki.ac.jp>
2022-05-11 19:43:43 +0900

    19_01

--- 2DLv1_2022_vs2019/GameProgramming/src/CCharacterManager.cpp
+++ 2DLv1_2022_vs2019/GameProgramming/src/CCharacterManager.cpp
@@ -1,31 +1,39 @@
 #include "CCharacterManager.h"
 
-CCharacterManager* CCharacterManager::Get()
-{
-	//static変数の作成
-	//staticは最初に一度作成され削除されない
-	static CCharacterManager cm;
-	//CCharacterManagerのインスタンスcmのポインタを返す
-	return &cm;
-}
+//CCharacterManager* CCharacterManager::Get()
+//{
+//	static変数の作成
+//	staticは最初に一度作成され削除されない
+//	static CCharacterManager cm;
+//	CCharacterManagerのインスタンスcmのポインタを返す
+//	return &cm;
+//}
 
 void CCharacterManager::Add(CCharacter* c)
 {
-	mCharacters.push_back(c);
+	mpCharacters.push_back(c);
 }
 
 void CCharacterManager::Update()
 {
-	for (size_t i = 0; i < mCharacters.size(); i++)
+	for (size_t i = 0; i < mpCharacters.size(); i++)
 	{
-		mCharacters[i]->Update();
+		mpCharacters[i]->Update();
 	}
+}
+
+void CCharacterManager::Collision()
+{
 }
 
 void CCharacterManager::Render()
 {
-	for (size_t i = 0; i < mCharacters.size(); i++)
+	for (size_t i = 0; i < mpCharacters.size(); i++)
 	{
-		mCharacters[i]->Render();
+		mpCharacters[i]->Render();
 	}
+}
+
+void CCharacterManager::Delete()
+{
 }
--- 2DLv1_2022_vs2019/GameProgramming/src/CCharacterManager.h
+++ 2DLv1_2022_vs2019/GameProgramming/src/CCharacterManager.h
@@ -8,19 +8,22 @@ class CCharacterManager
 {
 private:
 	//CCharacterのポインタの可変長配列
-	std::vector<CCharacter*> mCharacters;
+	std::vector<CCharacter*> mpCharacters;
 public:
 	//staticメソッドは最初から作成され、
 	//インスタンス不要で、どこでも呼ぶことができる。
 	//CCharacterManagerのインスタンスのポインタを返す
-	static CCharacterManager* Get();
+	//static CCharacterManager* Get();
+
 	//Add(CChracterのポインタ)
 	//可変長配列の後ろに追加する
 	void Add(CCharacter* c);
 	//可変長配列にあるポインタのUpdate()を実行していく
 	void Update();
-//	void Collision();
-//	void Delete();
+	//ポインタの衝突処理を実行する
+	void Collision();
 	//可変長配列にあるポインタのRender()を実行していく
 	void Render();
+	//不要なポインタを削除する
+	void Delete();
 };
