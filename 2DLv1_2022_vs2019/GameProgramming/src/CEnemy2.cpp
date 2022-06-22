df1fd526f90a2d627cd95ad59032bb3f24fc210d
Takahashi-Shinobu-Fukuyama <shinobu-takahashi@anabuki.ac.jp>
2022-06-22 10:53:24 +0900

    32

--- 2DLv1_2022_vs2019/GameProgramming/src/CEnemy2.cpp
+++ 2DLv1_2022_vs2019/GameProgramming/src/CEnemy2.cpp
@@ -11,14 +11,18 @@ void CEnemy2::Collision()
 
 void CEnemy2::Collision(CCharacter* m, CCharacter* o)
 {
+	//めり込み調整変数を宣言する
 	float x, y;
 	switch (o->Tag())
 	{
 	case ETag::ETURN:
+		//折り返しに当たった時
 		if (CRectangle::Collision(o, &x, &y))
 		{
+			//めり込まない位置まで戻す
 			X(X() + x);
 			Y(Y() + y);
+			//X軸速度を反転させる
 			mVx = -mVx;
 		}
 		break;
@@ -41,6 +45,7 @@ CEnemy2::CEnemy2(float x, float y, float w, float h, CTexture* pt)
 	Set(x, y, w, h);
 	Texture(pt, TEXCOORD);
 	mTag = ETag::EENEMY;
+	//X軸速度の初期値を移動速度にする
 	mVx = VELOCITY;
 }
 
@@ -53,6 +58,7 @@ void CEnemy2::Update()
 		Texture(Texture(), TEXCRY);
 		break;
 	case EState::EMOVE:
+		//X軸速度分、X座標を更新する
 		X(X() + mVx);
 		break;
 	}
