265e8d14eba485ffa2b060f655d8070f51daa69d
Takahashi-Shinobu-Fukuyama <shinobu-takahashi@anabuki-net.ne.jp>
2022-10-04 14:44:11 +0900

    23 VELOCITY

--- 3DLv1_2022_vs2019_2/GameProgramming/src/CEnemy.cpp
+++ 3DLv1_2022_vs2019_2/GameProgramming/src/CEnemy.cpp
@@ -1,5 +1,8 @@
 #include "CEnemy.h"
 
+//移動速度
+#define VELOCITY CVector(0.0f, 0.0f, 0.09f)
+
 //コンストラクタ
 //CEnemy(モデル, 位置, 回転, 拡縮)
 CEnemy::CEnemy(CModel* model, const CVector& position,
@@ -18,6 +21,5 @@ void CEnemy::Update() {
 	//行列を更新
 	CTransform::Update();
 	//位置を移動
-	mPosition = mPosition + CVector(0.0f, 0.0f, 0.09f) * mMatrixRotate;
-//	mPosition = CVector(0.0f, 0.0f, 0.9f) * mMatrix;
+	mPosition = mPosition + VELOCITY * mMatrixRotate;
 }
