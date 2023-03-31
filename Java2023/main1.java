b2aa92cf765c72e905d9fd1df3889d056f268b19
Takahashi-Shinobu-Fukuyama <shinobu-takahashi@anabuki.ac.jp>
2023-03-31 14:38:21 +0900

    Revert "ArrayList"
    This reverts commit e0d4ba0ca98e4acab07dd7bf124b04fce3f8915d.

--- Java2023/main1.java
+++ Java2023/main1.java
@@ -4,36 +4,12 @@ import javax.swing.*;
 	//.で区切られた最後がクラス名、それ以外はパッケージ名です。
 //import java.awt.Dimension;
 
+
 //import javax.swing.*;
 import java.awt.*;
 
-import java.util.*;
-
-class FillOval {
-	int x,y,w,h;
-	int r,g,b;
-	
-	FillOval(int x, int y, int w,int h,int r,int g,int b)
-	{
-		this.x = x;
-		this.y = y;
-		this.w = w;
-		this.h = h;
-		this.r = r;
-		this.g = g;
-		this.b = b;
-	}
-	void Render(Graphics gr)
-	{
-		Color color = new Color(r,g,b);//星の色
-		gr.setColor(color);//描画色の設定
-		gr.fillOval(x, y, w, h);//円の描画
-	}
-}
-
 	//JComponentを継承し、画面の部品を作成します
 class Gamen extends JComponent {
-	ArrayList<FillOval> mTaskList = new ArrayList<>();
 	int hsx;//星のX座標の変数を作成
 	int hsy;//星のY座標の変数を作成
 	int hsw;//星の幅の変数を作成
@@ -44,10 +20,7 @@ class Gamen extends JComponent {
 
 	//描画が必要なときに実行されるメソッド
 	public void paintComponent(Graphics g) {
-		mTaskList.add(new FillOval(30,40,50,60,255,255,0));
-		mTaskList.add(new FillOval(130,140,50,60,255,255,0));
-		mTaskList.add(new FillOval(230,240,50,60,255,255,0));
-		//描画色を青にする
+			//描画色を青にする
 		g.setColor(Color.black);
 			//X座標0、Y座標0から、幅150、高さ200の四角形を塗りつぶす
 		g.fillRect(0, 0, 300, 400);
@@ -84,26 +57,6 @@ class Gamen extends JComponent {
 		hsc = new Color(0,0,255);//緑色を作成
 		g.setColor(hsc);
 		g.fillOval(hsx, hsy, hsw, hsh);//2番目の円を描画
-
-
-		for(int i = 0; i < mTaskList.size(); i++)
-		{
-			System.out.println(i);
-			switch(i){
-				case 0:
-				mTaskList.get(0).Render(g);
-				mTaskList.remove(0);
-				break;
-				case 1:
-//				mTaskList.remove(1);
-				mTaskList.get(0).Render(g);
-				break;
-				case 2:
-				mTaskList.get(1).Render(g);
-				break;
-			}
-		}
-
 
 		g.setColor(Color.white);//描画色を白色にします
 		//X座標20、Y座標10にHello world!と文字列を描画します
