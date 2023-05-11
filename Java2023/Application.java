66a59d1ce9cc7578091d7ad224b1032562e46f74
Takahashi-Shinobu-Fukuyama <shinobu-takahashi@anabuki.ac.jp>
2023-05-11 18:50:05 +0900

    12T

--- Java2023/Application.java
+++ Java2023/Application.java
@@ -25,15 +25,14 @@ class Star {
 		c = color;
 	}
 
-	Star()
-	{
-		x = (int)(Math.random() * 300);
-		y = (int)(Math.random() * 400);
-		w = h = (int)(Math.random() * 100);
+	Star() {
+		x = (int) (Math.random() * 300);
+		y = (int) (Math.random() * 400);
+		w = h = (int) (Math.random() * 100);
 		c = new Color(
-			(int)(Math.random() * 256),
-			(int)(Math.random() * 256),
-			(int)(Math.random() * 256) );
+				(int) (Math.random() * 256),
+				(int) (Math.random() * 256),
+				(int) (Math.random() * 256));
 	}
 
 	// 描画する
@@ -42,51 +41,56 @@ class Star {
 		g.setColor(c);
 		g.fillOval(x, y, w, h);
 	}
+
+	//更新処理
+	void update() {
+		y += 1;
+	}
 }
 
 // JComponentを継承し、画面の部品を作成します
 class Screen extends JComponent {
-	//星クラスの作成
+	// 星クラスの作成
 	Star star;
 	Star star2;
 	Star star3;
-//	Star[] stars;
+	// Star[] stars;
 
-	//デフォルトコンストラクタの作成
+	// デフォルトコンストラクタの作成
 	Screen() {
 		// 各値をランダム値にします。
 		int ix, iy, iw, ih, ir, ig, ib;
-		ix = (int)(Math.random()) * 300;
-		iy = (int)(Math.random() * 400);
-		iw = ih = (int)(Math.random() * 64) + 1;
-		ir = (int)(Math.random() * 256);
-		ig = (int)(Math.random() * 256);
-		ib = (int)(Math.random() * 256);
-		star = new Star(ix,iy,iw,ih, new Color(ir, ig, ib));
+		ix = (int) (Math.random()) * 300;
+		iy = (int) (Math.random() * 400);
+		iw = ih = (int) (Math.random() * 64) + 1;
+		ir = (int) (Math.random() * 256);
+		ig = (int) (Math.random() * 256);
+		ib = (int) (Math.random() * 256);
+		star = new Star(ix, iy, iw, ih, new Color(ir, ig, ib));
 
-		ix = (int)(Math.random() * 300);
-		iy = (int)(Math.random() * 400);
-		iw = ih = (int)(Math.random() * 64) + 1;
-		ir = (int)(Math.random() * 256);
-		ig = (int)(Math.random() * 256);
-		ib = (int)(Math.random() * 256);
-		star2 = new Star(ix,iy,iw,ih, new Color(ir, ig, ib));
+		ix = (int) (Math.random() * 300);
+		iy = (int) (Math.random() * 400);
+		iw = ih = (int) (Math.random() * 64) + 1;
+		ir = (int) (Math.random() * 256);
+		ig = (int) (Math.random() * 256);
+		ib = (int) (Math.random() * 256);
+		star2 = new Star(ix, iy, iw, ih, new Color(ir, ig, ib));
 
-		ix = (int)(Math.random() * 300);
-		iy = (int)(Math.random() * 400);
-		iw = ih = (int)(Math.random() * 64) + 1;
-		ir = (int)(Math.random() * 256);
-		ig = (int)(Math.random() * 256);
-		ib = (int)(Math.random() * 256);
-		star3 = new Star(ix,iy,iw,ih, new Color(ir, ig, ib));
+		ix = (int) (Math.random() * 300);
+		iy = (int) (Math.random() * 400);
+		iw = ih = (int) (Math.random() * 64) + 1;
+		ir = (int) (Math.random() * 256);
+		ig = (int) (Math.random() * 256);
+		ib = (int) (Math.random() * 256);
+		star3 = new Star(ix, iy, iw, ih, new Color(ir, ig, ib));
 
-/*
-		stars = new Star[100];
-		for(int i = 0; i < stars.length; i++)
-		{
-			stars[i] = new Star();
-		}
-*/
+		/*
+		 * stars = new Star[100];
+		 * for(int i = 0; i < stars.length; i++)
+		 * {
+		 * stars[i] = new Star();
+		 * }
+		 */
 	}
 
 	// 描画が必要なときに実行されるメソッド
@@ -94,24 +98,49 @@ class Screen extends JComponent {
 		// 黒色で四角形を描画
 		g.setColor(Color.black);
 		g.fillRect(0, 0, 300, 400);
-		
+
 		// Starクラスのdrawメソッドを呼びます
 		star.draw(g);
 		star2.draw(g);
 		star3.draw(g);
 
-/*
-		for(int i = 0; i < stars.length; i++)
-		{
-			stars[i].draw(g);
-		}
-*/
+		/*
+		 * for(int i = 0; i < stars.length; i++)
+		 * {
+		 * stars[i].draw(g);
+		 * }
+		 */
 
 		// 白色で文字列を描画
 		g.setColor(Color.white);
 		g.drawString("Score", 10, 20);
 		g.drawString("High Score", 100, 20);
 	}
+
+	// loopメソッドを追加します
+	void loop() {
+		/*
+		 * while(条件)
+		 * 条件を満たす間、{ から } までの処理を繰り返します。
+		 * { } が無い場合、次の１命令だけを繰り返します。
+		 */
+		while (true) { // 条件が常にtrue（真）のため永久ループします
+			//更新処理を呼びます
+			star.update();
+			star2.update();
+			star3.update();
+
+			// 再描画します（paintComponentメソッドが呼び出されます）
+			repaint();
+
+			//停止します
+			try {
+				Thread.sleep(33); // 33ミリ秒停止します
+			} catch (InterruptedException e) {
+			}
+		}
+	}
+
 }
 
 public class Application {
@@ -144,5 +173,8 @@ public class Application {
 		frame.add(screen);
 
 		frame.setVisible(true);
+
+		screen.loop();
+
 	}
 }
