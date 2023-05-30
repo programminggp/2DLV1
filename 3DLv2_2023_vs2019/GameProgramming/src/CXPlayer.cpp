da1916b0b0be3a8f4166ad57f365614151f24164
Takahashi-Shinobu-Fukuyama <shinobu-takahashi@anabuki.ac.jp>
2023-05-30 17:43:37 +0900

    21

--- 3DLv2_2023_vs2019/GameProgramming/src/CXPlayer.cpp
+++ 3DLv2_2023_vs2019/GameProgramming/src/CXPlayer.cpp
@@ -2,19 +2,39 @@
 
 void CXPlayer::Update()
 {
-	if (mInput.Key('A')) {
-		mRotation.Y(mRotation.Y() + 2.0f);
-	}
-	if (mInput.Key('D')) {
-		mRotation.Y(mRotation.Y() - 2.0f);
-	}
-	if (mInput.Key('W')) {
-		ChangeAnimation(1, true, 60);
-		mPosition = mPosition + MatrixRotate().VectorZ() * 0.1f;
-	}
-	else
+	switch (AnimationIndex())
 	{
-		ChangeAnimation(0, true, 60);
+	case 3:
+		if (IsAnimationFinished())
+		{
+			ChangeAnimation(4, false, 30);
+		}
+		break;
+	case 4:
+		if (IsAnimationFinished())
+		{
+			ChangeAnimation(0, true, 60);
+		}
+		break;
+	default:
+		if (mInput.Key('A')) {
+			mRotation.Y(mRotation.Y() + 2.0f);
+		}
+		if (mInput.Key('D')) {
+			mRotation.Y(mRotation.Y() - 2.0f);
+		}
+		if (mInput.Key('W')) {
+			ChangeAnimation(1, true, 60);
+			mPosition = mPosition + MatrixRotate().VectorZ() * 0.1f;
+		}
+		else
+		{
+			ChangeAnimation(0, true, 60);
+		}
+		if (mInput.Key(' '))
+		{
+			ChangeAnimation(3, false, 30);
+		}
 	}
 	CXCharacter::Update();
 }
