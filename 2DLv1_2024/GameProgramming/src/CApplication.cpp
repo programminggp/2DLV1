8072e466f56dc7bc9bb1a15c1893cdcfff001dd7
snb-takahashi <snb.takahashi@gmail.com>
2023-06-04 17:48:08 +0900

    １．クラスの作成 ・クラスの定義 ・クラスのインスタンスの作成 ・メソッドの実行 ・インスタンスの破棄

--- 2DLv1_2024/GameProgramming/src/CApplication.cpp
+++ 2DLv1_2024/GameProgramming/src/CApplication.cpp
@@ -1,12 +1,55 @@
-#include "CApplication.h"
-
-void CApplication::Start()
-{
-
-}
-
-void CApplication::Update()
-{
-
-}
-
+//#pragma once
+//#include "glut.h"
+////
+////クラスの宣言
+////
+//class CApplication;
+//class CRect;
+//
+////
+////クラスの定義
+////
+//class CRect
+//{
+//public:
+//	void Render();
+//};
+//
+//void CRect::Render()
+//{
+//	glBegin(GL_QUADS);
+//	glVertex2f(0.0f, 0.0f);
+//	glVertex2f(0.0f, 0.0f);
+//	glVertex2f(0.0f, 0.0f);
+//	glVertex2f(0.0f, 0.0f);
+//	glEnd();
+//}
+//
+//
+//class CApplication
+//{
+//public:
+//	//最初に一度だけ実行するメソッド
+//	void Start();
+//	//繰り返し実行するメソッド
+//	void Update();
+//	//デストラクタ
+//	~CApplication();
+//private:
+//	CRect* mpRect;
+//};
+//
+//void CApplication::Start()
+//{
+//	mpRect = new CRect();
+//}
+//
+//void CApplication::Update()
+//{
+//	mpRect->Render();
+//}
+//
+//CApplication::~CApplication()
+//{
+//	delete mpRect;
+//}
--- 2DLv1_2024/GameProgramming/src/CApplication.h
+++ 2DLv1_2024/GameProgramming/src/CApplication.h
@@ -1,10 +1,55 @@
 #pragma once
+#include "glut.h"
+//
+//クラスの宣言
+//
+class CApplication;
+class CRect;
+
+//
+//クラスの定義
+//
+class CRect
+{
+public:
+	void Render();
+};
+
+void CRect::Render()
+{
+	glBegin(GL_QUADS);
+	glVertex2f(0.0f, 0.0f);
+	glVertex2f(400.0f, 0.0f);
+	glVertex2f(400.0f, 300.0f);
+	glVertex2f(0.0f, 300.0f);
+	glEnd();
+}
+
 
 class CApplication
 {
 public:
-	//最初に一度だけ実行するプログラム
+	//最初に一度だけ実行するメソッド
 	void Start();
-	//繰り返し実行するプログラム
+	//繰り返し実行するメソッド
 	void Update();
+	//デストラクタ
+	~CApplication();
+private:
+	CRect* mpRect;
 };
+
+void CApplication::Start()
+{
+	mpRect = new CRect();
+}
+
+void CApplication::Update()
+{
+	mpRect->Render();
+}
+
+CApplication::~CApplication()
+{
+	delete mpRect;
+}
