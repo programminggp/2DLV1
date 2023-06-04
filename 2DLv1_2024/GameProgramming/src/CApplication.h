4ba2e147626516b6506c23b298591d4bcd5d903a
snb-takahashi <snb.takahashi@gmail.com>
2023-06-04 17:52:19 +0900

    1.

--- 2DLv1_2024/GameProgramming/src/CApplication.h
+++ 2DLv1_2024/GameProgramming/src/CApplication.h
@@ -9,14 +9,19 @@ class CRect;
 //
 //クラスの定義
 //
+//四角形クラスの定義
 class CRect
 {
 public:
+	//メソッドの宣言
+	//描画処理
 	void Render();
 };
 
+//四角形クラスの描画処理の定義
 void CRect::Render()
 {
+	//画面に四角形を描画する
 	glBegin(GL_QUADS);
 	glVertex2f(0.0f, 0.0f);
 	glVertex2f(400.0f, 0.0f);
@@ -36,20 +41,24 @@ public:
 	//デストラクタ
 	~CApplication();
 private:
+	//インスタンス変数の宣言
 	CRect* mpRect;
 };
 
 void CApplication::Start()
 {
+	//インスタンスの生成
 	mpRect = new CRect();
 }
 
 void CApplication::Update()
 {
+	//メソッドの呼び出し
 	mpRect->Render();
 }
 
 CApplication::~CApplication()
 {
+	//インスタンスの削除
 	delete mpRect;
 }
