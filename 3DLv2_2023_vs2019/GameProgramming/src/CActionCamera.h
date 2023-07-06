f080532492da9615c9775ecd42411c3571320bf9
Takahashi-Shinobu-Fukuyama <shinobu-takahashi@anabuki.ac.jp>
2023-07-06 18:34:02 +0900

    30

---
+++ 3DLv2_2023_vs2019/GameProgramming/src/CActionCamera.h
@@ -0,0 +1,41 @@
+#ifndef CACTIONCAMERA_H
+#define CACTIONCAMERA_H
+#include "CVector.h"
+#include "CTransform.h"
+#include "CMatrix.h"
+#include "CInput.h"
+/*
+カメラクラス
+*/
+class CActionCamera : public CTransform {
+public:
+	CActionCamera();
+	//インスタンスの取得
+	static CActionCamera* Instance();
+	//カメラのX軸取得
+	CVector VectorX();
+	//カメラのZ軸取得
+	CVector VectorZ();
+	//設定メソッド(距離, X回転,Y回転)
+	void Set(float distance, float xaxis, float yaxis);
+	//カメラ更新
+	void Update();
+	//カメラ適用
+	void Render();
+private:
+	//インスタンス
+	static CActionCamera* spInstance;
+	//視点
+	CVector mEye;
+	//注視点
+	CVector mCenter;
+	//上方向
+	CVector mUp;
+	//モデルビュー行列
+	CMatrix mModelView;
+	//
+	CInput mInput;
+};
+
+#endif
+
--- 3DLv2_2023_vs2019/GameProgramming/src/CXPlayer.cpp
+++ 3DLv2_2023_vs2019/GameProgramming/src/CXPlayer.cpp
@@ -1,4 +1,5 @@
 #include "CXPlayer.h"
+#include "CActionCamera.h"
 
 CXPlayer::CXPlayer()
 	: mColSphereBody(this, nullptr, CVector(), 0.5f)
@@ -22,6 +23,17 @@ void CXPlayer::Init(CModelX* model)
 
 void CXPlayer::Update()
 {
+	//カメラの前方
+	CVector cameraZ = CActionCamera::Instance()->VectorZ();
+	//カメラの左方向
+	CVector cameraX = CActionCamera::Instance()->VectorX();
+	//キャラクタの前方
+	CVector charZ = mMatrixRotate.VectorZ();
+	//XZ平面にして正規化
+	cameraZ.Y(0.0f); cameraZ = cameraZ.Normalize();
+	cameraX.Y(0.0f); cameraX = cameraX.Normalize();
+	charZ.Y(0.0f); charZ = charZ.Normalize();
+
 	switch (AnimationIndex())
 	{
 	case 3:
@@ -37,24 +49,57 @@ void CXPlayer::Update()
 		}
 		break;
 	default:
+		//向きの設定
+		CVector move;
 		if (mInput.Key('A')) {
-			mRotation.Y(mRotation.Y() + 2.0f);
+			move = move + cameraX;
 		}
 		if (mInput.Key('D')) {
-			mRotation.Y(mRotation.Y() - 2.0f);
+			move = move - cameraX;
 		}
 		if (mInput.Key('W')) {
-			ChangeAnimation(1, true, 60);
-			mPosition = mPosition + MatrixRotate().VectorZ() * 0.1f;
+			move = move + cameraZ;
+		}
+		if (mInput.Key('S')) {
+			move = move - cameraZ;
+		}
+		//向きあり
+		if (move.Length() > 0.0f)
+		{
+			//遊び
+			const float MARGIN = 0.06f;
+			//正規化
+			move = move.Normalize();
+			//自分の向きと向かせたい向きで外積
+			float cross = charZ.Cross(move).Y();
+			//自分の向きと向かせたい向きで内積
+			float dot = charZ.Dot(move);
+			//外積がプラスは左回転
+			if (cross > MARGIN) {
+				mRotation.Y(mRotation.Y() + 5.0f);
+			}
+			//外積がマイナスは右回転
+			else if (cross < -MARGIN) {
+				mRotation.Y(mRotation.Y() - 5.0f);
+			}
+			//前後の向きが同じとき内積は1.0
+			else if (dot < 1.0f - MARGIN) {
+				mRotation.Y(mRotation.Y() - 5.0f);
+			}
+//			ChangeAnimation(1, true, 60);
+//			mPosition = mPosition + move * 0.1f;
 		}
 		else
 		{
 			ChangeAnimation(0, true, 60);
 		}
+
 		if (mInput.Key(' '))
 		{
 			ChangeAnimation(3, false, 30);
 		}
+
+
 	}
 	CXCharacter::Update();
 }
