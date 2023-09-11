29fdec6eeba3260b9ad9c4c3f375be9e9f705e1f
Takahashi-Shinobu-Fukuyama <shinobu-takahashi@anabuki.ac.jp>
2023-09-11 17:03:03 +0900

    01

--- 2DLv1_v2/GameProgramming/src/CApplication.cpp
+++ 2DLv1_v2/GameProgramming/src/CApplication.cpp
@@ -1,8 +1,28 @@
 #include "CApplication.h"
+#include <stdio.h> //printf関数のインクルード
+
+//四角形クラスの宣言
+class CRect;
+
+//四角形クラスの定義
+class CRect
+{
+public:
+	void Render(); //Renderメソッドの宣言
+};
+
+
+//Renderメソッドの定義
+void CRect::Render()
+{
+	printf("Render CRect\n"); //文字列の出力
+}
+
 
 void CApplication::Start()
 {
-
+	CRect rect; //CRectクラスのインスタンスrectの作成
+	rect.Render(); //rectインスタンスのRender()を実行
 }
 
 void CApplication::Update()
