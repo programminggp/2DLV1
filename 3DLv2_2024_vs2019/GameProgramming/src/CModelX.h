b7046494c4008612c9490b5d1a871b61fdb49f8c
Takahashi-Shinobu-Fukuyama <shinobu-takahashi@anabuki-net.en.jp>
2024-03-04 18:08:14 +0900

    1.Finished

---
+++ 3DLv2_2024_vs2019/GameProgramming/src/CModelX.h
@@ -0,0 +1,22 @@
+#ifndef CMODELX_H	//インクルードガード
+#define CMODELX_H
+
+#define MODEL_FILE "res\\sample.blend.x"	//入力ファイル名 \は￥
+
+//領域解放をマクロ化
+#define SAFE_DELETE_ARRAY(a) { if(a) delete[] a; a = nullptr;}
+/*
+ CModelX
+ Xファイル形式の3Dモデルデータをプログラムで認識する
+*/
+class CModelX {
+public:
+	CModelX();
+	//ファイル読み込み
+	void Load(char* file);
+private:
+	char* mpPointer;	//読み込み位置
+	char mToken[1024];	//取り出した単語の領域
+};
+
+#endif
