fc434c1176b5a04b341687eac8ccac1001d2c03c
Takahashi-Shinobu-Fukuyama <shinobu-takahashi@anabuki-net.en.jp>
2024-03-07 11:30:16 +0900

    3.Fin

--- 3DLv2_2024_vs2019/GameProgramming/src/CModelX.cpp
+++ 3DLv2_2024_vs2019/GameProgramming/src/CModelX.cpp
@@ -3,6 +3,36 @@
 #include "CModelX.h"
 #include "glut.h"
 
+CModelX::~CModelX()
+{
+	if (mFrame.size() > 0)
+	{
+		delete mFrame[0];
+	}
+}
+
+/*
+SkipNode
+ノードを読み飛ばす
+*/
+void CModelX::SkipNode() {
+	//文字が終わったら終了
+	while (*mpPointer != '\0') {
+		GetToken();	//次の単語取得
+		//{が見つかったらループ終了
+		if (strchr(mToken, '{')) break;
+	}
+	int count = 1;
+	//文字が終わるか、カウントが0になったら終了
+	while (*mpPointer != '\0' && count > 0) {
+		GetToken();	//次の単語取得
+		//{を見つけるとカウントアップ
+		if (strchr(mToken, '{')) count++;
+		//}を見つけるとカウントダウン
+		else if (strchr(mToken, '}')) count--;
+	}
+}
+
 /*
 GetToken
 文字列データから、単語を1つ取得する
@@ -83,14 +113,8 @@ void CModelX::Load(char* file) {
 		GetToken();	//単語の取得
 		//単語がFrameの場合
 		if (strcmp(mToken, "Frame") == 0) {
-			printf("%s ", mToken);	//Frame出力
-			GetToken();	//Frame名を取得
-			printf("%s\n", mToken); //Frame名を出力
-		}
-		if (strcmp(mToken, "AnimationSet") == 0) {
-			printf("%s ", mToken);	//Frame出力
-			GetToken();	//Frame名を取得
-			printf("%s\n", mToken); //Frame名を出力
+			//フレームを作成する
+			new CModelXFrame(this);
 		}
 	}
 
@@ -122,4 +146,62 @@ bool CModelX::IsDelimiter(char c)
 	return false;
 }
 
+/*
+ CModelXFrame
+ model：CModelXインスタンスへのポインタ
+ フレームを作成する
+ 読み込み中にFrameが見つかれば、フレームを作成し、
+ 子フレームに追加する
+*/
+CModelXFrame::CModelXFrame(CModelX* model)
+	: mpName(nullptr)
+	, mIndex(0)
+{
+	//現在のフレーム配列の要素数を取得し設定する
+	mIndex = model->mFrame.size();
+	//CModelXのフレーム配列に追加する
+	model->mFrame.push_back(this);
+	//変換行列を単位行列にする
+	mTransformMatrix.Identity();
+	//次の単語（フレーム名の予定）を取得する
+	model->GetToken(); // frame name
+	//フレーム名分エリアを確保する
+	mpName = new char[strlen(model->mToken) + 1];
+	//フレーム名をコピーする
+	strcpy(mpName, model->mToken);
+	//次の単語（{の予定）を取得する
+	model->GetToken();  // {
+	//文字が無くなったら終わり
+	while (*model->mpPointer != '\0') {
+		//次の単語取得
+		model->GetToken(); // Frame
+		//}かっこの場合は終了
+		if (strchr(model->mToken, '}')) break;
+		//新なフレームの場合は、子フレームに追加
+		if (strcmp(model->mToken, "Frame") == 0) {
+			//フレームを作成し、子フレームの配列に追加
+			mChild.push_back(
+				new CModelXFrame(model));
+		}
+		else {
+			//上記以外の要素は読み飛ばす
+			model->SkipNode();
+		}
+	}
+	//デバッグバージョンのみ有効
+#ifdef _DEBUG
+	printf("%s\n", mpName);
+#endif
+}
+
+CModelXFrame::~CModelXFrame()
+{
+	//子フレームを全て解放する
+	std::vector<CModelXFrame*>::iterator itr;
+	for (itr = mChild.begin(); itr != mChild.end(); itr++) {
+		delete* itr;
+	}
+	//名前のエリアを解放する
+	SAFE_DELETE_ARRAY(mpName);
+}
 
--- 3DLv2_2024_vs2019/GameProgramming/src/CModelX.h
+++ 3DLv2_2024_vs2019/GameProgramming/src/CModelX.h
@@ -1,7 +1,27 @@
 #ifndef CMODELX_H	//インクルードガード
 #define CMODELX_H
+#include <vector>		//vectorクラスのインクルード（動的配列）
+#include "CMatrix.h"	//マトリクスクラスのインクルード
+class CModelX;			// CModelXクラスの宣言
+class CModelXFrame;		// CModelXFrameクラスの宣言
 
 #define MODEL_FILE "res\\sample.blend.x"	//入力ファイル名 \は￥
+
+//CModelXFrameクラスの定義
+class CModelXFrame {
+	friend CModelX;
+public:
+	//コンストラクタ
+	CModelXFrame(CModelX* model);
+	//デストラクタ
+	~CModelXFrame();
+private:
+	std::vector<CModelXFrame*> mChild;  //子フレームの配列
+	CMatrix mTransformMatrix;  //変換行列
+	char* mpName;   //フレーム名前
+	int mIndex;  //フレーム番号
+};
+
 
 //領域解放をマクロ化
 #define SAFE_DELETE_ARRAY(a) { if(a) delete[] a; a = nullptr;}
@@ -10,7 +30,12 @@
  Xファイル形式の3Dモデルデータをプログラムで認識する
 */
 class CModelX {
+	friend CModelXFrame;
 public:
+	~CModelX();
+	//ノードの読み飛ばし
+	void SkipNode();
+
 	//単語の取り出し
 	char* GetToken();
 
@@ -18,6 +43,7 @@ public:
 	//ファイル読み込み
 	void Load(char* file);
 private:
+	std::vector<CModelXFrame*> mFrame;  //フレームの配列
 	//cが区切り文字ならtrueを返す
 	bool IsDelimiter(char c);
 
