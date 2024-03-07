da1fc0f66854ce1ea185337d8ac16c65e6b529ad
Takahashi-Shinobu-Fukuyama <shinobu-takahashi@anabuki-net.en.jp>
2024-03-07 11:19:15 +0900

    2.Fin

--- 3DLv2_2024_vs2019/GameProgramming/src/CModelX.h
+++ 3DLv2_2024_vs2019/GameProgramming/src/CModelX.h
@@ -11,10 +11,16 @@
 */
 class CModelX {
 public:
+	//単語の取り出し
+	char* GetToken();
+
 	CModelX();
 	//ファイル読み込み
 	void Load(char* file);
 private:
+	//cが区切り文字ならtrueを返す
+	bool IsDelimiter(char c);
+
 	char* mpPointer;	//読み込み位置
 	char mToken[1024];	//取り出した単語の領域
 };
