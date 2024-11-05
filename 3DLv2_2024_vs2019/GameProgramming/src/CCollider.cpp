17776f1811a6b1838fd404f5de9bbf52f49df872
Takahashi <shinobu-takahashi@anabuki-net.ne.jp>
2024-11-05 12:08:10 +0900

    Update CCollisionCapsuleTriangle Add CollisionTriagleLine2

--- 3DLv2_2024_vs2019/GameProgramming/src/CCollider.cpp
+++ 3DLv2_2024_vs2019/GameProgramming/src/CCollider.cpp
@@ -2,6 +2,65 @@
 #include "CCollisionManager.h"
 #include "CColliderLine.h"
 
+bool CollisionTriangleLine2(const CVector& tv0, const CVector& tv1, const CVector& tv2, const CVector& lv0, const CVector& lv1, CVector* adjust)
+{
+	//面の法線を、外積を正規化して求める
+	CVector normal = (tv1 - tv0).Cross(tv2 - tv0).Normalize();
+	//三角の頂点から線分始点へのベクトルを求める
+	CVector v0sv = lv0 - tv0;
+	//三角の頂点から線分終点へのベクトルを求める
+	CVector v0ev = lv1 - tv0;
+	//線分が面と交差しているか内積で確認する
+	float dots = v0sv.Dot(normal);
+	float dote = v0ev.Dot(normal);
+	//プラスは交差してない
+	if (dots * dote >= 0.0f) {
+		//衝突してない（調整不要）
+		*adjust = CVector(0.0f, 0.0f, 0.0f);
+		return false;
+	}
+
+	//線分は面と交差している
+	//面と線分の交点を求める
+	//交点の計算
+	CVector cross = lv0 + (lv1 - lv0) * (abs(dots) / (abs(dots) + abs(dote)));
+
+	//交点が三角形内なら衝突している
+	//頂点1頂点2ベクトルと頂点1交点ベクトルとの外積を求め、
+	//法線との内積がマイナスなら、三角形の外
+	if ((tv1 - tv0).Cross(cross - tv0).Dot(normal) < 0.0f) {
+		//衝突してない
+		*adjust = CVector(0.0f, 0.0f, 0.0f);
+		return false;
+	}
+	//頂点2頂点3ベクトルと頂点2交点ベクトルとの外積を求め、
+	//法線との内積がマイナスなら、三角形の外
+	if ((tv2 - tv1).Cross(cross - tv1).Dot(normal) < 0.0f) {
+		//衝突してない
+		*adjust = CVector(0.0f, 0.0f, 0.0f);
+		return false;
+	}
+	//頂点3頂点1ベクトルと頂点3交点ベクトルとの外積を求め、
+	//法線との内積がマイナスなら、三角形の外
+	if ((tv0 - tv2).Cross(cross - tv2).Dot(normal) < 0.0f) {
+		//衝突してない
+		*adjust = CVector(0.0f, 0.0f, 0.0f);
+		return false;
+	}
+
+	//調整値計算（衝突しない位置まで戻す）
+	if (dots < 0.0f) {
+		//始点が裏面
+		*adjust = normal * -dots;
+	}
+	else {
+		//終点が裏面
+		*adjust = normal * -dote;
+	}
+	return true;
+}
+
+
 void CCollider::ChangePriority(int priority)
 {
 	mPriority = priority;
@@ -21,9 +80,11 @@ bool CCollider::CollisionTriangleSphere(CCollider* t, CCollider* s, CVector* a)
 	//線コライダをワールド座標で作成
 	sv = s->mPosition * *s->mpMatrix + normal * s->mRadius;
 	ev = s->mPosition * *s->mpMatrix - normal * s->mRadius;
-	CColliderLine line(nullptr, nullptr, sv, ev);
+	return CollisionTriangleLine2(v[0], v[1], v[2], sv, ev, a);
+
+//	CColliderLine line(nullptr, nullptr, sv, ev);
 	//三角コライダと線コライダの衝突処理
-	return CollisionTriangleLine(t, &line, a);
+//	return CollisionTriangleLine(t, &line, a);
 }
 
 CCollider::CCollider()
@@ -321,11 +382,28 @@ bool CCollider::CollisionCapsuleTriangle(CCollider* m, CCollider* t, CVector* a)
 	v[0] = t->mV[0] * *t->mpMatrix;
 	v[1] = t->mV[1] * *t->mpMatrix;
 	v[2] = t->mV[2] * *t->mpMatrix;
+	//面の法線を、外積を正規化して求める
+	CVector normal = (v[1] - v[0]).Cross(v[2] - v[0]).Normalize();
+
+	//線コライダをワールド座標で作成
+	sv = m->V(0) + normal * m->mRadius;
+	ev = m->V(0) - normal * m->mRadius;
+	if (CollisionTriangleLine2(v[0], v[1], v[2], sv, ev, a))
+	{
+		return true;
+	}
+
+	sv = m->V(1) + normal * m->mRadius;
+	ev = m->V(1) - normal * m->mRadius;
+	if (CollisionTriangleLine2(v[0], v[1], v[2], sv, ev, a))
+	{
+		return true;
+	}
+
 	CVector r = ((m->V(0) - m->V(1)).Normalize()) * m->mRadius;
 	sv = m->V(0) + r;
 	ev = m->V(1) - r;
-	//面の法線を、外積を正規化して求める
-	CVector normal = (v[1] - v[0]).Cross(v[2] - v[0]).Normalize();
+
 	//三角の頂点から線分始点へのベクトルを求める
 	CVector v0sv = sv - v[0];
 	//三角の頂点から線分終点へのベクトルを求める
